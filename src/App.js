4fb7bf478d021f1fa263e1972ecfdf6a99c662fe
jqzy91 <jamesquek91@live.com.sg>
2022-09-18 03:22:29 +0800

    base clock react practice

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import logo from "./logo.png";
 import "./App.css";
+import Clock from "./components/Clock.js";
 
 class App extends React.Component {
   render() {
@@ -8,9 +9,10 @@ class App extends React.Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+
+          <Clock timeZone="Asia/Singapore" />
+          <Clock timeZone="Europe/London" />
+          <Clock timeZone="America/Phoenix" />
         </header>
       </div>
     );
